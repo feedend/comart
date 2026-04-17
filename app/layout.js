3257447f9f09b845f54c43465dc842339a982794
feedend <questforfeed@gmail.com>
2026-04-17 19:42:26 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'FeedEnd PWA',
+  description: 'Sistema di feedback anonimo',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  )
+}
