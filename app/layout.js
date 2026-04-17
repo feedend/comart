8633ce9136bcbaa25fc9d7e3776e824c1d71ff44
feedend <questforfeed@gmail.com>
2026-04-17 23:26:56 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import "./globals.css";
+import './globals.css'
 
 export const metadata = {
-  title: "COMART Feedback",
-  description: "Sistema di valutazione corsi",
-};
+  title: 'Comart Feedback',
+  description: 'Creato con amore',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
-  );
+  )
 }
